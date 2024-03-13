7b98ced0a44208047242eea13d5e93a5def85221
Jeanette S <nova777nova@hotmail.com>
2024-03-13 18:28:39 +0100

    Changed my list to only names. I had not read the instructions completly and missed the secon paper of the exercise with the NameRepository. Sorry for the trouble!

--- src/main/java/se/JavaLexicon/ArrayNamesWs.java
+++ src/main/java/se/JavaLexicon/ArrayNamesWs.java
@@ -3,25 +3,27 @@ package se.JavaLexicon;
 public class ArrayNamesWs {
 
     private static String[] names = new String[4];
-    private static int birthdays = 0;
+    private static int index =0;
 
     public static void main (String[] args) {
-        addName ("Nicklas Soderman", "19610911");
-        addName ("Jeanette Soderman", "19730507");
-        addName ("Josefin Soderman", "20010224");
-        addName ("Carolin Soderman", "20060606");
+        addName ("Nicklas Soderman");
+        addName ("Jeanette Soderman");
+        addName ("Josefin Soderman");
+        addName ("Carolin Soderman");
 
-        printNames ();
+    printNames();
     }
 
-    private static void addName (String fullName, String birthday) {
-        if (birthdays < names.length) {
-            names[birthdays] = fullName + " " + birthday;
-            birthdays++;
+    private static void addName (String fullName) {
+        if (index >= names.length) {
+            System.out.println("The Array is full, can not add more names");
+            return;
         }
+        names[index] = fullName;
+        index++;
     }
 
-    private static void printNames () {
+    private static void printNames() {
         for (String name : names) {
             if (name != null) {
                 System.out.println (name);
