f0441ca30c8fc9e4508ddf61b9aca983488cec76
Jeanette S <nova777nova@hotmail.com>
2024-03-13 19:41:05 +0100

    added part 2 to the Namerepository

--- src/main/java/se/JavaLexicon/NameRepository.java
+++ src/main/java/se/JavaLexicon/NameRepository.java
@@ -1,24 +1,32 @@
 package se.JavaLexicon;
 
     public class NameRepository {
-    private static String[] names = new String[0];
+        private static String[] names = new String[0];
 
-    public static int getSize()  {
-    return names.length;
+        public static int getSize () {
+            return names.length;
+        }
+
+        public static void addName (String[] names) {
+            NameRepository.names = names;
+
+        }
+
+        public static void clear () {
+            String[] names = new String[0];
+
+        }
+
+        public static String[] findAll () {
+            return names.clone ();
+        }
+
+        public static String find (final String fullName) {
+            return null;
+
+        }
+        public static boolean add(final String fullName) {
+            return false;
+        }
+
     }
-    public static void addName(String[] names) {
-    NameRepository.names = names;
-
-    }
-
-    public static void clear () {
-        String[] names = new String[0];
-
-    }
-    public static String[] findAll() {
-    return names.clone ();
-    }
-    }
-
-
-
